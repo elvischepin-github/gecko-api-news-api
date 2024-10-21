c5ad34cd373f47f33c60df47c91a4bebad2c2abf
elvischepin-github <hello.elvis@icloud.com>
2024-10-21 13:59:13 +0300

    Komitas

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Button from "./button.jsx";
+
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ src/button.jsx
@@ -0,0 +1,7 @@
+import "./button.css";
+
+function Button() {
+  return <button>Hello</button>;
+}
+
+export default Button;
