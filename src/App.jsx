353bece8aabef76ebe71fd7050b37b7bd47be4bc
elvischepin-github <hello.elvis@icloud.com>
2024-10-23 00:35:50 +0300

    Komitas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import NewsBlock from "./components/newsBlock.jsx";
 
-const GeckoAPI = process.env.REACT_APP_GECKO_API;
-const NewsAPI = process.env.REACT_APP_NEWS_API;
-
-// <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-
+// const GeckoAPI = process.env.REACT_APP_GECKO_API;
 // const url = `https://api.coingecko.com/api/v3/coins/list?x_cg_demo_api_key=${GeckoAPI}`;
-
 // const options = { method: "GET", headers: { accept: "application/json" } };
-
 // fetch(url, options)
 //   .then((res) => res.json())
 //   .then((json) => console.log(json))
 //   .catch((err) => console.error(err));
-//GECKO||GECKO||GECKO||GECKO||GECKO||GECKO||GECKO||GECKO||
 
 function App() {
-  return <NewsBlock />;
+  return (
+    <>
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+      <NewsBlock />
+    </>
+  );
 }
 
 export default App;
--- src/components/newsBlock.jsx
+++ src/components/newsBlock.jsx
@@ -1,3 +1,20 @@
+const NewsAPI = process.env.REACT_APP_NEWS_API;
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NewsAPI}`;
+const options = { method: "GET", headers: { accept: "application/json" } };
+
+async function getApiData() {
+  try {
+    const response = await fetch(url, options);
+    const moldingToJson = await response.json();
+
+    return moldingToJson;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getApiData();
+
 function NewsBlock() {
   return (
     <div
