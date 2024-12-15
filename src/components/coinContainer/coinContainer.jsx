0129aece9726766babe46d23552696259d4accc0
elvischepin-github <hello.elvis@icloud.com>
2024-12-15 02:37:30 +0200

    update

---
+++ src/components/coinContainer/coinContainer.jsx
@@ -0,0 +1,6 @@
+import CoinBlock from "../coinBlock/coinBlock";
+
+function CoinContainer() {
+  return <></>;
+}
+export default CoinContainer;
---
+++ src/components/newsContainer/newsContainer.jsx
@@ -0,0 +1,13 @@
+import NewsBlock from "../newsBlock/newsBlock";
+
+import "./newsContainer.css";
+
+function NewsContainer() {
+  return (
+    <div className="newsContainer">
+      <NewsBlock />
+    </div>
+  );
+}
+
+export default NewsContainer;
