cff99d30021bd91622dbfb078bcf0f19389c938d
elvischepin-github <hello.elvis@icloud.com>
2025-05-23 17:20:35 +0300

    build config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,11 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     define: {
-      "process.env.REACT_APP_GECKO_API": JSON.stringify(
-        env.REACT_APP_GECKO_API
-      ),
-      "process.env.REACT_APP_NEWS_API": JSON.stringify(env.REACT_APP_NEWS_API),
+      "process.env.REACT_APP_GECKO_API": JSON.stringify(env.VITE_GECKO_API),
+      "process.env.REACT_APP_NEWS_API": JSON.stringify(env.VITE_NEWS_API),
       "process.env.NODE_ENV": JSON.stringify(mode),
     },
+
     plugins: [react()],
     build: {
       minify: "esbuild",
